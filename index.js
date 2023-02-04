9c7bf8d4c1c80bb191e0111185af15ac9fe6fc48
find4omis <124463537+find4omis@users.noreply.github.com>
2023-02-04 14:41:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ function onLoad(){
         for (var c=0;c<10;++c){
             var cell = tr.appendChild(document.createElement('td'));
             cell.innerHTML = ++i;
-            cell.addEventListener('click',() => (console.log("hey"));
+            cell.addEventListener('click', help);
         }
     }
     document.body.appendChild(grid);
 }
+
+function help(){
+	console.log("hey queen");
+}
+
 
 function onClick(event){
     var img = document.createElement('img');
